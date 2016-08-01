26560663fdb5e8dd76088fb0e9ad8fc576566d16
tuannd255 <tuannd255.com>
2016-08-01 14:31:05 +0700

    admin edit and destroy subject

--- app/controllers/admin/subjects_controller.rb
+++ app/controllers/admin/subjects_controller.rb
@@ -1,6 +1,10 @@
 class Admin::SubjectsController < ApplicationController
   load_and_authorize_resource
 
+  def index
+    @subjects = @subjects.page params[:page]
+  end
+
   def new
   end
 
@@ -11,6 +15,30 @@ class Admin::SubjectsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @subject.update_attributes subject_params
+      flash[:success] = t "subjects.update.success"
+      redirect_to admin_subject_path @subject
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @subject.destroy
+      flash[:success] = t "subjects.delete.success"
+    else
+      flash[:success] = t "subjects.delete.fail"
+    end
+    redirect_to admin_subjects_path
   end
 
   private
