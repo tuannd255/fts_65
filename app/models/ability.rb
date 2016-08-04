4b40d125c96440a8fa0b18587172f7cc7324af84
tuannd255 <tuannd255.com>
2016-08-04 08:33:53 +0700

    finish user do exam 2

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,11 +6,11 @@ class Ability
     if user.is_admin?
       can :read, :all
       can :manage, Subject
-      can [:edit, :destroy], User do |other_user|
+      can [:edit, :update, :destroy], User do |other_user|
         user != other_user
       end
     else
-      can [:read, :create], Exam
+      can [:read, :create, :update], Exam
       if namespace == "admin"
         cannot :manage, :all
       end
